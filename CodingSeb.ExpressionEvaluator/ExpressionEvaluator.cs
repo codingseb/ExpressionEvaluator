ed7231db76624e865e33dc5ce1b55e46fdcf7f5b
CodingSeb <sebgeiser@gmail.com>
2020-06-04 11:17:58 +0200

    inc version for unstable

--- CodingSeb.ExpressionEvaluator/ExpressionEvaluator.cs
+++ CodingSeb.ExpressionEvaluator/ExpressionEvaluator.cs
@@ -1,6 +1,6 @@
 /******************************************************************************************************
     Title : ExpressionEvaluator (https://github.com/codingseb/ExpressionEvaluator)
-    Version : 1.4.10.0 
+    Version : 1.4.10.1 
     (if last digit (the forth) is not a zero, the version is an intermediate version and can be unstable)
 
     Author : Coding Seb
